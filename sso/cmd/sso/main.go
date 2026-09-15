df8affd144d3ff61d00327b17288916b62ee6878
kirill <petrov3466@mail.ru>
2026-09-15 14:12:38 +0500

    setup + cfg

---
+++ sso/cmd/sso/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"log/slog"
+	"os"
+	"sso/internal/config"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
+func main() {
+	// conf
+
+	cfg := config.MustLoad()
+
+	// logger
+	log := setupLogger(cfg.Env)
+	log.Info(
+		"starting sso server",
+		slog.Any("cfg", cfg),
+	)
+
+	// init
+
+	// grpc server
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+			Level: slog.LevelDebug,
+		}))
+	case envDev:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+			Level: slog.LevelDebug,
+		}))
+	case envProd:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+			Level: slog.LevelInfo,
+		}))
+	}
+
+	return log
+}
